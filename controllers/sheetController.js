bbdf137a0b2981b939582ba4962d7cafc1cdecb7
Pete <Pete.Light19@gmail.com>
2024-02-02 18:41:16 +0000

    added media queries and pagination

--- controllers/sheetController.js
+++ controllers/sheetController.js
@@ -11,22 +11,37 @@ router.get('/', (req, res) => {
 router.get('/search', async (req, res) => {
   try {
     const query = (req.query.query || '').toLowerCase();
+    const page = parseInt(req.query.page) || 1;
+    const itemsPerPage = 20; // Set the maximum results per page
 
     // Fetch data if not already loaded
     if (!res.app.locals.sheetData) {
-      await fetchData('Sheet1'); // Specify the sheet name
+      await fetchData('Sheet1');
     }
 
     const data = res.app.locals.sheetData || [];
 
-    // Filter data based on the user's search query (customize this based on your data structure)
+    // Filter data based on the user's search query
     const filteredData = data.filter((row) =>
       Object.values(row || {}).some((cell) =>
         (cell || '').toLowerCase().includes(query)
       )
     );
 
-    res.render('pages/search', { data: filteredData });
+    // Paginate the results
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const paginatedData = filteredData.slice(startIndex, endIndex);
+
+    res.render('pages/search', {
+      data: paginatedData,
+      currentPage: page,
+      totalPages: Math.ceil(filteredData.length / itemsPerPage),
+      query: req.query.query,
+      page: page,
+      itemsPerPage: itemsPerPage,
+      filteredData: filteredData,
+    });
   } catch (error) {
     console.error('Error fetching or rendering data:', error);
     res.status(500).send('Internal Server Error');
